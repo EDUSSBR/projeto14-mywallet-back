612ada3a14cd0491a881b8e3845422f6da447e59
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-21 09:39:16 -0300

    refactored for parsing token to controllers

--- src/middlewares/authenticateUser.js
+++ src/middlewares/authenticateUser.js
@@ -4,29 +4,27 @@ export async  function authenticateUserMiddleware(req, res, next) {
     try { 
         const { authorization, id } = req.headers
         const token = authorization?.replace("Bearer ", "")
-        console.log(token)
-        if (token === undefined || token.length === 0 || !uuidValidateV4(token)) {
-            return res.status(401).send("Token invalido")
+        const isNotAvalidToken = token === undefined || token.length === 0 || !uuidValidateV4(token)
+        if (isNotAvalidToken) {
+            return res.status(401).send("Invalid token")
         }
-        console.log(token)
-        const userTokens = await accountRepository.getTokensByID(id)
-        let tokenExists= false;
-        console.log("TOKEN",userTokens)
-        for (let item of userTokens.token) {
-            console.log("item for",item)
+        const user = await accountRepository.getUserByID(id)
+        let tokenExistsInDatabase= false;
+        for (let item of user.token) {
             if (item === token){
-                tokenExists=true
+                tokenExistsInDatabase=true
             }
         }
-        if (!tokenExists){
-            return res.status(401).send("Token invalido")
+        if (!tokenExistsInDatabase){
+            return res.status(401).send("Invalid token")
         }
+        req.token=token
         next()
     } catch (e) {
         if (e.status) {
             return res.status(e.status).send(e.message)
         }
-        console.log(e)
+        console.error(e)
         res.sendStatus(500)
     }
 }
