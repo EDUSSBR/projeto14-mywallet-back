f0ddf834ab765235900c3e06fa973557a6536804
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-21 09:40:09 -0300

    add return message for validation errors

--- src/middlewares/validateAccountInfo.js
+++ src/middlewares/validateAccountInfo.js
@@ -9,12 +9,22 @@ const accountSchema = Joi.object({
 
 export function validateAccountInfo(req, res, next){
         try{
+            const isEverithingRequiredNotSent = req.body.nome === undefined || req.body.email === undefined || req.body.senha === undefined
+            if (isEverithingRequiredNotSent) {
+                throw { message: "please make a valid request, all fields are required.", status: 422 }
+            }
             const [nome, email, senha] = strip(req.body.nome, req.body.email, req.body.senha);
             const newAccount = accountSchema.validate({nome, email, senha},  { abortEarly: false })
             if (newAccount.error) throw {message: newAccount.error.details.map(item=>item.message), status: 422}
             next() 
         } catch (e) {
-            res.status(e.status).send(e.message)
+            if (e.status) {
+                return res.status(e.status).send(e.message)
+            }
+            console.log("A not found Error appeared")
+            console.error(e)
+            res.status(500).send(e.message)
+    
         }
 }
 
