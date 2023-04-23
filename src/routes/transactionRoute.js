1c851525dc0021e82e82cdafa71dc6faab4573a3
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-23 10:21:04 -0300

    change transaction for fitting naming convention

--- src/routes/transactionRoute.js
+++ src/routes/transactionRoute.js
@@ -6,12 +6,13 @@ import { getTransactionService } from '../services/getTransactionsService.js';
 
 const router = new express.Router()
 
-router.post("/nova-transacao/:tipo", authenticateUserMiddleware, validateTransactionInfo, async (req, res) => {
+router.post("/transaction/:tipo", authenticateUserMiddleware, validateTransactionInfo, async (req, res) => {
     try {
         const { id } = req.headers
         const { tipo } = req.params
         const { value, desc } = req.body
         await executeTransaction(id, value, desc, tipo)
+        console.log("##############",tipo)
         res.send("Transaction created!")
     } catch (e) {
         if (e.status) {
@@ -23,11 +24,8 @@ router.post("/nova-transacao/:tipo", authenticateUserMiddleware, validateTransac
     }
 })
 
-router.get("/nova-transacao", authenticateUserMiddleware, async (req, res) => {
+router.get("/transaction", authenticateUserMiddleware, async (req, res) => {
     try {
-
-        console.log(req.headers.id)
-        console.log(req.coco)
         const transactions = await getTransactionService(req.headers.id)
         res.send(transactions)
     } catch (e) {
