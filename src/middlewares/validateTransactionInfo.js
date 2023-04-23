20daaa62ef53c9c96d70b664686a7a0dd9dee567
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-23 10:22:24 -0300

    add some parse function strategy for dealing with transactions value

--- src/middlewares/validateTransactionInfo.js
+++ src/middlewares/validateTransactionInfo.js
@@ -2,7 +2,7 @@ import Joi from "joi"
 import { strip } from "../helpers/strip.js"
 
 const accountSchema = Joi.object({
-    value: Joi.number().positive().precision(2).required(),
+    value: Joi.number().precision(2).required(),
     desc: Joi.string().trim().min(3).required()
 })
 
@@ -13,7 +13,8 @@ export function validateTransactionInfo(req, res, next) {
             throw { message: "please make a valid request, all fields are required.", status: 422 }
         }
         const [value, desc] = strip(req.body?.value, req.body?.desc);
-        const newAccount = accountSchema.validate({ value, desc }, { abortEarly: false })
+        const parsedValue = Number(String(value).replace(",", ".").replace("-", "").replace("+",""))
+        const newAccount = accountSchema.validate({ value: parsedValue, desc }, { abortEarly: false })
         if (newAccount.error) throw { message: newAccount.error.details.map(item => item.message), status: 422 }
         next()
     } catch (e) {
