4d1655090e275d87f490ff5a45507f56b96f6807
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-25 10:14:39 -0300

    NADA

--- src/app.js
+++ src/app.js
@@ -2,4 +2,4 @@ import app from "./routes/index.js";
 import dotenv from 'dotenv';
 dotenv.config()
 
-app.listen(process.env.PORT, ()=>console.log(`Server listening at ${process.env.PORT}...`))
+app.listen(process.env.PORT, ()=>console.log(`Server running on port ${process.env.PORT}...`))
